a5622cec96627d219d21ae861ccac49f911f4264
Tonyk <tonykurosaki117@gmail.com>
2022-02-16 20:49:36 -0600

    first commit

---
+++ src/views/atoms/Text/MenuText/MenuText.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './MenuText.css';
+
+export const MenuText = ({content}) => {
+    return(
+        <>
+            <h4 className="menu-text">{content}</h4>
+        </>
+    )
+}
